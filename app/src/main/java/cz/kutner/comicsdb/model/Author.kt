9f42534c19fc19d683e7c6bb5592d2b48566ad99
Lukáš Kutner <lukas@kutner.cz>
2015-10-27 14:50:36 +0100

    načítání detailu autora

--- app/src/main/java/cz/kutner/comicsdb/model/Author.kt
+++ app/src/main/java/cz/kutner/comicsdb/model/Author.kt
@@ -2,6 +2,8 @@ package cz.kutner.comicsdb.model
 
 import java.util.*
 
-public data class Author(public val name: String?, public val country: String?, private val id: Int?) {
-    private val comicses: ArrayList<Comics>? = null
+public data class Author(public val name: String?, public val country: String?, public val id: Int?) {
+    public var comicses: ArrayList<Comics>? = null
+    public var bio: String? = null
+    public var notes: String? = null
 }
