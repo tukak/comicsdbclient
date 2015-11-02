8a425acc891332d82137e15861841a0726fec693
Kutner Lukas <Lukas.Kutner@hcg.homecredit.net>
2015-11-02 07:54:18 +0100

    testy detailu komiksu

--- app/src/test/java/cz/kutner/comicsdb/ConverterTest.kt
+++ app/src/test/java/cz/kutner/comicsdb/ConverterTest.kt
@@ -1,17 +1,13 @@
 package cz.kutner.comicsdb
 
-import org.junit.Test
-
-import java.io.File
-
 import cz.kutner.comicsdb.connector.converter.AuthorDetailConverter
-import cz.kutner.comicsdb.connector.converter.ComicsConverter
+import cz.kutner.comicsdb.connector.parser.ComicsParser
 import cz.kutner.comicsdb.model.Author
-import cz.kutner.comicsdb.model.Comics
+import org.junit.Assert.assertEquals
+import org.junit.Test
 import retrofit.converter.ConversionException
 import retrofit.mime.TypedString
-
-import org.junit.Assert.assertEquals
+import java.io.File
 
 
 class ConverterTest {
@@ -32,11 +28,14 @@ class ConverterTest {
 
     @Test
     fun ComicsDetailTest() {
-        val typedInput = TypedString(readFile("/comics_300.html"))
         try {
-            val comics_300 = ComicsConverter().fromBody(typedInput, Comics::class.java) as Comics
+            val comics_300 = ComicsParser().parseComicsDetail(readFile("/comics_300.html").byteInputStream(), "utf-8")
             assertEquals("300", comics_300.name)
+            assertEquals("pevná", comics_300.binding)
             assertEquals("325 x 245 mm", comics_300.format)
+            assertEquals("barevný", comics_300.print)
+            assertEquals("999 Kč", comics_300.price)
+            assertEquals("historický, dobrodružný, drama", comics_300.genre)
             assertEquals(94, comics_300.rating)
         } catch (e: ConversionException) {
             e.printStackTrace()
