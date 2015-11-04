be7498b40bc358c45290c9cc8a3c949dd2d00a33
Lukáš Kutner <lukas@kutner.cz>
2015-11-04 12:27:19 +0100

    test na photo url

--- app/src/test/java/cz/kutner/comicsdb/ConverterTest.kt
+++ app/src/test/java/cz/kutner/comicsdb/ConverterTest.kt
@@ -17,6 +17,7 @@ class ConverterTest {
             assertEquals("USA", miller.country)
             assertEquals(335, miller.id)
             assertEquals(44, miller.comicses.count())
+            assertEquals("http://comicsdb.cz/picautor/xid335.jpg.pagespeed.ic.52Q4ZzHlgf.jpg", miller.photoUrl)
         } catch (e: ConversionException) {
             e.printStackTrace()
         }
