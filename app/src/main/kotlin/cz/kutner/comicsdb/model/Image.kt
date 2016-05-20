f715db323d8e6cc6bb2790d4f5d37d65efda339d
Lukáš Kutner <lukas@kutner.cz>
2016-05-20 10:10:37 +0200

    Image bude parcelable

--- app/src/main/kotlin/cz/kutner/comicsdb/model/Image.kt
+++ app/src/main/kotlin/cz/kutner/comicsdb/model/Image.kt
@@ -1,4 +1,30 @@
 package cz.kutner.comicsdb.model
 
-data class Image(var previewUrl:String, var fullUrl:String, var caption:String) {
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Image(var previewUrl:String, var fullUrl:String, var caption:String) : Parcelable {
+    constructor(source: Parcel): this(source.readString(), source.readString(), source.readString())
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest?.writeString(previewUrl)
+        dest?.writeString(fullUrl)
+        dest?.writeString(caption)
+    }
+
+    companion object {
+        @JvmField final val CREATOR: Parcelable.Creator<Image> = object : Parcelable.Creator<Image> {
+            override fun createFromParcel(source: Parcel): Image {
+                return Image(source)
+            }
+
+            override fun newArray(size: Int): Array<Image?> {
+                return arrayOfNulls(size)
+            }
+        }
+    }
 }
