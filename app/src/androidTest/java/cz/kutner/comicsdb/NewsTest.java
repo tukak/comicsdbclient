428d9690ac8a61138f1533e8f23a5b16f4f4f27a
Lukáš Kutner <lukas@kutner.cz>
2016-07-19 14:31:11 +0200

    nahrazení deprecated @LargeTest

--- app/src/androidTest/java/cz/kutner/comicsdb/NewsTest.java
+++ app/src/androidTest/java/cz/kutner/comicsdb/NewsTest.java
@@ -4,7 +4,7 @@ package cz.kutner.comicsdb;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
+import android.support.test.filters.LargeTest;
 
 import org.junit.Rule;
 import org.junit.Test;
