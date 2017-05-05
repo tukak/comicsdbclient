c9c258303d90a7e41b16e24aacbfee54a6c22153
Lukáš Kutner <lukas.kutner@embedit.cz>
2017-05-05 12:46:46 +0200

    idling resource

--- app/src/main/kotlin/cz/kutner/comicsdb/di/RetrofitModule.kt
+++ app/src/main/kotlin/cz/kutner/comicsdb/di/RetrofitModule.kt
@@ -5,13 +5,13 @@ import cz.kutner.comicsdb.connector.service.*
 import retrofit2.Retrofit
 
 class RetrofitModule(baseUrl: String) {
-    val seriesListService: SeriesListService by lazy { Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(SeriesListConverterFactory.create()).build().create(SeriesListService::class.java) }
-    val seriesDetailService: SeriesDetailService by lazy { Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(SeriesDetailConverterFactory.create()).build().create(SeriesDetailService::class.java) }
-    val authorDetailService: AuthorDetailService by lazy { Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(AuthorDetailConverterFactory.create()).build().create(AuthorDetailService::class.java) }
-    val authorListService: AuthorListService by lazy { Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(AuthorListConverterFactory.create()).build().create(AuthorListService::class.java) }
-    val classifiedService: ClassifiedService by lazy { Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(ClassifiedConverterFactory.create()).build().create(ClassifiedService::class.java) }
-    val comicsDetailService: ComicsDetailService by lazy { Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(ComicsDetailConverterFactory.create()).build().create(ComicsDetailService::class.java) }
-    val comicsListService: ComicsListService by lazy { Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(ComicsListConverterFactory.create()).build().create(ComicsListService::class.java) }
-    val forumService: ForumService by lazy { Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(ForumConverterFactory.create()).build().create(ForumService::class.java) }
-    val newsService: NewsService by lazy { Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(NewsConverterFactory.create()).build().create(NewsService::class.java) }
+    val seriesListService: SeriesListService by lazy { Retrofit.Builder().baseUrl(baseUrl).client(OkHttpProvider.okHttpClient).addConverterFactory(SeriesListConverterFactory.create()).build().create(SeriesListService::class.java) }
+    val seriesDetailService: SeriesDetailService by lazy { Retrofit.Builder().baseUrl(baseUrl).client(OkHttpProvider.okHttpClient).addConverterFactory(SeriesDetailConverterFactory.create()).build().create(SeriesDetailService::class.java) }
+    val authorDetailService: AuthorDetailService by lazy { Retrofit.Builder().baseUrl(baseUrl).client(OkHttpProvider.okHttpClient).addConverterFactory(AuthorDetailConverterFactory.create()).build().create(AuthorDetailService::class.java) }
+    val authorListService: AuthorListService by lazy { Retrofit.Builder().baseUrl(baseUrl).client(OkHttpProvider.okHttpClient).addConverterFactory(AuthorListConverterFactory.create()).build().create(AuthorListService::class.java) }
+    val classifiedService: ClassifiedService by lazy { Retrofit.Builder().baseUrl(baseUrl).client(OkHttpProvider.okHttpClient).addConverterFactory(ClassifiedConverterFactory.create()).build().create(ClassifiedService::class.java) }
+    val comicsDetailService: ComicsDetailService by lazy { Retrofit.Builder().baseUrl(baseUrl).client(OkHttpProvider.okHttpClient).addConverterFactory(ComicsDetailConverterFactory.create()).build().create(ComicsDetailService::class.java) }
+    val comicsListService: ComicsListService by lazy { Retrofit.Builder().baseUrl(baseUrl).client(OkHttpProvider.okHttpClient).addConverterFactory(ComicsListConverterFactory.create()).build().create(ComicsListService::class.java) }
+    val forumService: ForumService by lazy { Retrofit.Builder().baseUrl(baseUrl).client(OkHttpProvider.okHttpClient).addConverterFactory(ForumConverterFactory.create()).build().create(ForumService::class.java) }
+    val newsService: NewsService by lazy { Retrofit.Builder().baseUrl(baseUrl).client(OkHttpProvider.okHttpClient).addConverterFactory(NewsConverterFactory.create()).build().create(NewsService::class.java) }
 }
