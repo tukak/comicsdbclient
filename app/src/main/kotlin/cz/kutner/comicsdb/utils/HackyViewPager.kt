e4179075d5bcd9b0a15ffad971ab3ef1bdc9942b
Lukáš Kutner <lukas.kutner@embedit.cz>
2017-06-26 15:52:02 +0200

    oprava pádu při vyhledávání

--- app/src/main/kotlin/cz/kutner/comicsdb/utils/HackyViewPager.kt
+++ app/src/main/kotlin/cz/kutner/comicsdb/utils/HackyViewPager.kt
@@ -2,9 +2,10 @@ package cz.kutner.comicsdb.utils
 
 import android.content.Context
 import android.support.v4.view.ViewPager
+import android.util.AttributeSet
 import android.view.MotionEvent
 
-class HackyViewPager(context: Context) : ViewPager(context) {
+class HackyViewPager(context: Context, attributeSet: AttributeSet) : ViewPager(context, attributeSet) {
     override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
         try {
             return super.onInterceptTouchEvent(ev)
