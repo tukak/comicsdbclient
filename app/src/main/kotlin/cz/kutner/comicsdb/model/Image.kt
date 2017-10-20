fedc584f868478b56adb4ea445f83e392b30b819
Kutner Lukas <Lukas.Kutner@embedit.cz>
2017-10-20 07:07:18 +0200

    @Parcelize Image

--- app/src/main/kotlin/cz/kutner/comicsdb/model/Image.kt
+++ app/src/main/kotlin/cz/kutner/comicsdb/model/Image.kt
@@ -1,31 +1,7 @@
 package cz.kutner.comicsdb.model
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
-data class Image(val previewUrl:String, val fullUrl:String, val caption:String) : Parcelable {
-    constructor(source: Parcel): this(source.readString(), source.readString(), source.readString())
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    override fun writeToParcel(dest: Parcel?, flags: Int) {
-        dest?.writeString(previewUrl)
-        dest?.writeString(fullUrl)
-        dest?.writeString(caption)
-    }
-
-    companion object {
-        @Suppress("unused")
-        @JvmField val CREATOR: Parcelable.Creator<Image> = object : Parcelable.Creator<Image> {
-            override fun createFromParcel(source: Parcel): Image {
-                return Image(source)
-            }
-
-            override fun newArray(size: Int): Array<Image?> {
-                return arrayOfNulls(size)
-            }
-        }
-    }
-}
+@Parcelize
+data class Image(val previewUrl: String, val fullUrl: String, val caption: String) : Parcelable
