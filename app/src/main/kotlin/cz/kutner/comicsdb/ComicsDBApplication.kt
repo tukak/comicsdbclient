82603bcc0368baa654910602194ac6999df898f3
Kutner Lukas <Lukas.Kutner@embedit.cz>
2018-01-11 14:20:32 +0100

    crashlytics pouze na produkčních buildech

--- app/src/main/kotlin/cz/kutner/comicsdb/ComicsDBApplication.kt
+++ app/src/main/kotlin/cz/kutner/comicsdb/ComicsDBApplication.kt
@@ -1,12 +1,16 @@
 package cz.kutner.comicsdb
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
 import com.google.firebase.perf.metrics.AddTrace
 import cz.kutner.comicsdb.di.SERVER_URL
 import cz.kutner.comicsdb.di.koinModule
 import org.koin.android.ext.android.bindString
 import org.koin.android.ext.android.startKoin
 import timber.log.Timber
+import io.fabric.sdk.android.Fabric
+
+
 
 class ComicsDBApplication : Application() {
 
@@ -17,6 +21,8 @@ class ComicsDBApplication : Application() {
         bindString(R.string.url_comicsdb, SERVER_URL)
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
+        } else {
+            Fabric.with(this, Crashlytics())
         }
     }
 }
