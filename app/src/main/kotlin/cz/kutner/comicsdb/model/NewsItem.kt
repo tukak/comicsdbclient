ebd977a54e29e96852cbb1c8d2628753ffe103ce
Kutner Lukas <Lukas.Kutner@embedit.cz>
2018-06-04 07:59:36 +0200

    zobrazování obrázků v novinkách

--- app/src/main/kotlin/cz/kutner/comicsdb/model/NewsItem.kt
+++ app/src/main/kotlin/cz/kutner/comicsdb/model/NewsItem.kt
@@ -1,10 +1,8 @@
 package cz.kutner.comicsdb.model
 
-import android.text.Spanned
-import androidx.core.text.parseAsHtml
 import java.util.*
 
 data class NewsItem(val title: String?, val nick: String, val text: String, val time: Date) : Item {
-    fun getTextFromHtml(): Spanned? =
-        text.replace("href='/", "href='http://www.comicsdb.cz/").parseAsHtml()
+    fun getTextWithUrl(): String =
+        text.replace("href='/", "href='http://www.comicsdb.cz/")
 }
